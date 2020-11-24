da4f4193ce19f1efafe75bce2126c27bd9cf7e35
Maciej <maciej@cosphere.org>
2020-11-24 16:26:05 +0100

    fix pypi package

--- setup.py
+++ setup.py
@@ -4,8 +4,26 @@ import os.path
 from setuptools import setup, find_packages
 
 
-requirements_path = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+#
+# REQUIREMENTS
+#
+def parse_requirements(requirements):
+
+    return [
+        r.strip()
+        for r in requirements
+        if (
+            not r.strip().startswith('#') and
+            not r.strip().startswith('-e') and
+            r.strip())
+    ]
+
+
+with open(os.path.join(BASE_DIR, 'requirements.txt')) as f:
+    requirements = parse_requirements(f.readlines())
 
 
 setup(
@@ -13,14 +31,23 @@ setup(
     description=(
         'Lily extension for managing environment variables, '
         'their validity etc.'),
-    version='0.0.3',
-    author='CoSphere Tech Team',
-    url='https://bitbucket.org/goodai/lily-env',
+    version='0.0.4',
+    url='https://github.com/cosphere-org/lily-env',
+    author='CoSphere Tech',
+    author_email='contact@cosphere.org',
     packages=find_packages(),
+    data_files=[(
+        '',
+        [
+            'requirements.txt',
+            'README.md',
+        ],
+    )],
+    include_package_data=True,
     entry_points='''
         [console_scripts]
         lily_env=lily_env.cli:cli
     ''',
-    install_requires=open(requirements_path).readlines(),
-    package_data={'': ['requirements.txt']},
-    include_package_data=True)
+    install_requires=requirements,
+    keywords=['lily', 'django', 'drf'],
+    classifiers=[])
